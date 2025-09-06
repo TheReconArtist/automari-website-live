5fd61d4a4362f4a78d9b4f53e6f5cacd7aeb25eb
TheReconArtist <themelodicmelody@gmail.com>
2025-09-06 05:58:48 -0400

    Add placeholder MariMariChatbot component

---
+++ app/components/Chatbot/MariMariChatbot.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MariMariChatbot.css';
+
+const MariMariChatbot = () => {
+    return (
+        <div className="mari-mari-chatbot">
+            <p>MariMari Chatbot Coming Soon!</p>
+        </div>
+    );
+};
+
+export default MariMariChatbot;
